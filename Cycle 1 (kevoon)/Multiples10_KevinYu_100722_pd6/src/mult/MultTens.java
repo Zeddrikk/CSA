8224fb4c9d5a6c85b604afc51c7e696d77f3da4a
Zeddrikk <74263353+Zeddrikk@users.noreply.github.com>
2022-10-07 14:40:34 -0500

    Add files via upload

---
+++ Cycle 1 (kevoon)/Multiples10_KevinYu_100722_pd6/src/mult/MultTens.java
@@ -0,0 +1,20 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+package mult;
+public class MultTens
+{
+public static String go(  int x )
+{
+	String out = "";
+	if(x<=0) {
+		return "CVHS";
+	}
+	for(int y =0; y<=x; y++) {
+		int temp = y*10;
+		out = out+temp;
+		
+	}
+return out;
+}
+}
