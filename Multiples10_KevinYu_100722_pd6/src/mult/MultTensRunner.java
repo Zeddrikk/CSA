ca64c2083effdc7562fde92c619ac4e34a3e5ce1
Zeddrikk <74263353+Zeddrikk@users.noreply.github.com>
2022-10-07 14:40:09 -0500

    Add files via upload

---
+++ Multiples10_KevinYu_100722_pd6/src/mult/MultTensRunner.java
@@ -0,0 +1,23 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+package mult;
+
+public class MultTensRunner
+{
+public static void main( String[] args )
+{
+//add more test cases
+//samples from doc
+System.out.println( MultTens.go( 1 ) );
+System.out.println( MultTens.go( 2 ) );
+System.out.println( MultTens.go( 13 ) );
+System.out.println( MultTens.go( 4 ) );
+System.out.println( MultTens.go( 10 ) );
+System.out.println( MultTens.go( -5 ) );
+System.out.println( MultTens.go( 0 ) );
+System.out.println( MultTens.go( 6 ) );
+
+
+}
+}
